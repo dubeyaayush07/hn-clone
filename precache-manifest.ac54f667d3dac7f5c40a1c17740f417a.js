14529c570a122cf3829c5a96ade0d667bdc2230b
dubeyaayush07 <dubeyaayush07@gmail.com>
2019-11-02 15:16:08 +0530

    Updates

---
+++ precache-manifest.ac54f667d3dac7f5c40a1c17740f417a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a0ced27d82e8e4ed037ec7dc2fc99d",
+    "url": "/hn-clone/index.html"
+  },
+  {
+    "revision": "c2fc0e5245f9b37032c1",
+    "url": "/hn-clone/static/css/2.65d0fb34.chunk.css"
+  },
+  {
+    "revision": "9ed0ccbd58575aaed0bf",
+    "url": "/hn-clone/static/css/main.4675ecba.chunk.css"
+  },
+  {
+    "revision": "c2fc0e5245f9b37032c1",
+    "url": "/hn-clone/static/js/2.bf2523de.chunk.js"
+  },
+  {
+    "revision": "9ed0ccbd58575aaed0bf",
+    "url": "/hn-clone/static/js/main.2edc7437.chunk.js"
+  },
+  {
+    "revision": "f8a0abe02245ae1c8fca",
+    "url": "/hn-clone/static/js/runtime-main.f24cc566.js"
+  }
+]);
